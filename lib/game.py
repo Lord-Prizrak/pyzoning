0569cea755985254fd05862adc6df89315fb14ca
Admin <Admin@WS07401008005>
2012-01-13 17:12:18 +1100

    ????? ???? class.py ? ??? ????? ????? ??????? ???????.

--- lib/game.py
+++ lib/game.py
@@ -107,36 +107,3 @@ class Game:
         font = pygame.font.Font(None, 20)
         text = font.render( str(self.clock.get_fps())+" "+str(self.clock.get_time()), 1, (250, 250, 250) )
         self.screen.blit(text, (10,self.SCREEN_SIZE[1]-20))
-
-
-class Planet:
-    """ Клас планеты. """
-    
-    select = False
-    
-    def __init__(self):
-        """ Инициализируемся. """
-        resc = loadres()
-        image = resc.rndImage("planet")
-        self.image = pygame.transform.scale(image, (30,30) )
-        self.image.set_colorkey( (0,0,0), pygame.RLEACCEL )
-        self.rect = self.image.get_rect()
-        
-    def set_pos(self, pos):
-        """ Устанавливаем позицию. В координатах основной поверхности! """
-        self.rect.center = pos
-        
-    def update(self):
-        """ Обновляем объект. """
-        if self.select:
-            if self.image.get_alpha() is None:
-                self.image.set_alpha(100)
-        else:
-            if self.image.get_alpha() is not None:
-                self.image.set_alpha()
-
-
-class Siur:
-    """ Класс сиура. Т.е. энерго-объединения планет в блоки по шесть. """
-    def __init__(self):
-        pass
